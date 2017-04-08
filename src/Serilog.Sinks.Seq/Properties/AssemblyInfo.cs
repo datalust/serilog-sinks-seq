25d9a8576c85e5b6494a972737257ba6fbf206d0
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-04-08 21:42:33 +1000

    Move to VS2017 tooling

--- src/Serilog.Sinks.Seq/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.Seq/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Serilog.Sinks.Seq")]
-[assembly: AssemblyDescription("Serilog sink for Seq Events")]
-[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2013-2016")]
+[assembly: AssemblyVersion("3.0.0.0")]
+
 [assembly: InternalsVisibleTo("Serilog.Sinks.Seq.Tests, PublicKey=" +
                               "0024000004800000940000000602000000240000525341310004000001000100fb8d13fd344a1c" +
                               "6fe0fe83ef33c1080bf30690765bc6eb0df26ebfdf8f21670c64265b30db09f73a0dea5b3db4c9" +

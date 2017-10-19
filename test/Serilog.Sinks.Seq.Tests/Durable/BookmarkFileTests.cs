78e0d805fb1e78a3eade6e7351f618f258e70b6d
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-10-19 16:30:17 +1000

    Roll buffer files on size in durable mode (#92)
    * Fixes #52 - roll buffer file on size (uses pre-release Serilog.Sinks.File)
    
    Changes the semantics of the parameter previously called bufferFileSizeLimitBytes - this is now bufferSizeLimitBytes and is applied to the whole file set.
    
    * Make sure debug/verbose logs make it through
    
    * Boost durable mode test coverage
    
    * Abandon resx; fix test
    
    * Trying to figure out encoding differences causing CI failure
    
    * Update to VS2017 build image
    
    *  Jump through some hoops to try to get consistent test results
    
    * Revert broken byte count
    
    * Might as well make text x-plat

---
+++ test/Serilog.Sinks.Seq.Tests/Durable/BookmarkFileTests.cs
@@ -0,0 +1,25 @@
+﻿using Serilog.Sinks.Seq.Durable;
+using Serilog.Sinks.Seq.Tests.Support;
+using Xunit;
+
+namespace Serilog.Sinks.Seq.Tests.Durable
+{
+    public class BookmarkFileTests
+    {
+        [Fact]
+        public void BookmarkPersistenceCanBeRoundTripped()
+        {
+            using (var tmp = new TempFolder())
+            {
+                var position = new FileSetPosition(1234, Some.String());
+
+                var bookmark = new BookmarkFile(tmp.AllocateFilename("bookmark"));
+                bookmark.WriteBookmark(position);
+
+                var read = bookmark.TryReadBookmark();
+                Assert.Equal(position.NextLineStart, read.NextLineStart);
+                Assert.Equal(position.File, read.File);
+            }
+        }
+    }
+}
---
+++ test/Serilog.Sinks.Seq.Tests/Durable/PayloadReaderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Serilog.Sinks.Seq.Durable;
+using Serilog.Sinks.Seq.Tests.Support;
+using Xunit;
+using IOFile = System.IO.File;
+
+namespace Serilog.Sinks.Seq.Tests.Durable
+{
+    public class PayloadReaderTests
+    {
+        [Fact]
+        public void ReadsEventsFromBufferFiles()
+        {
+            using (var tmp = new TempFolder())
+            {
+                var fn = tmp.AllocateFilename("json");
+                var lines = IOFile.ReadAllText(Path.Combine("Resources", "ThreeBufferedEvents.json.txt"), Encoding.UTF8).Split(new [] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+                using (var f = IOFile.Create(fn))
+                using (var fw = new StreamWriter(f, Encoding.UTF8))
+                {
+                    foreach (var line in lines)
+                    {
+                        fw.WriteLine(line);
+                    }
+                }
+                var position = new FileSetPosition(0, fn);
+                var count = 0;
+                var payload = PayloadReader.ReadPayload(1000, null, ref position, ref count);
+
+                Assert.Equal(3, count);
+                Assert.Equal(576 + 3 * (Environment.NewLine.Length - 1), position.NextLineStart);
+                Assert.Equal(fn, position.File);
+
+                var data = JsonConvert.DeserializeObject<dynamic>(payload);
+                var events = data["Events"];
+                Assert.NotNull(events);
+                Assert.Equal(3, events.Count);
+            }
+        }
+    }
+}

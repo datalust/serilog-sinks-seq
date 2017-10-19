cbc14cabd51d5ed3de36ed0fd9c659f2cd9baae3
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-10-19 16:19:28 +1000

    Jump through some hoops to try to get consistent test results

--- test/Serilog.Sinks.Seq.Tests/Durable/PayloadReaderTests.cs
+++ test/Serilog.Sinks.Seq.Tests/Durable/PayloadReaderTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using Newtonsoft.Json;
 using Serilog.Sinks.Seq.Durable;
@@ -16,14 +17,21 @@ namespace Serilog.Sinks.Seq.Tests.Durable
             using (var tmp = new TempFolder())
             {
                 var fn = tmp.AllocateFilename("json");
-                var lines = IOFile.ReadAllText(Path.Combine("Resources", "ThreeBufferedEvents.json.txt"), Encoding.UTF8);
-                IOFile.WriteAllText(fn, lines, Encoding.UTF8);
+                var lines = IOFile.ReadAllText(Path.Combine("Resources", "ThreeBufferedEvents.json.txt"), Encoding.UTF8).Split(new [] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+                using (var f = IOFile.Create(fn))
+                using (var fw = new StreamWriter(f, Encoding.UTF8))
+                {
+                    foreach (var line in lines)
+                    {
+                        fw.WriteLine(line);
+                    }
+                }
                 var position = new FileSetPosition(0, fn);
                 var count = 0;
                 var payload = PayloadReader.ReadPayload(1000, null, ref position, ref count);
 
                 Assert.Equal(3, count);
-                Assert.Equal(579, position.NextLineStart);
+                Assert.Equal(576 + 3 * Environment.NewLine.Length - 1, position.NextLineStart);
                 Assert.Equal(fn, position.File);
 
                 var data = JsonConvert.DeserializeObject<dynamic>(payload);
